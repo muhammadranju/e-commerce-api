2464bd66eda55d0acc7cb6cb1f101b9d5b2ca758
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-02-20 14:33:35 +0600

    add DatabaseConnection.js

---
+++ src/config/DatabaseConnection.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const { DATABASE_NAME, DATABASE_QUERY } = require("../constant");
+
+const MONGODB_URI = process.env.MONGODB_URI; // REPLACE WITH YOUR OWN MONGODB URI
+const MONGODB_URI_LOCAL = process.env.MONGODB_URI_LOCAL; // REPLACE WITH YOUR LOCAL MONGODB URI
+
+async function connectDB() {
+  try {
+    const connectInstance = await mongoose.connect(
+      `${MONGODB_URI_LOCAL}/${DATABASE_NAME}`
+    );
+
+    // const connectInstance = await mongoose.connect(
+    //   `${MONGODB_URI}/${DATABASE_NAME}${DATABASE_QUERY}`
+    // );
+    console.log(
+      `MongoDB connected!! DB HOST: ${connectInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("MONGODB CONNECTION FIELD: ", error);
+    process.exit(1);
+  }
+}
+
+module.exports = connectDB;
