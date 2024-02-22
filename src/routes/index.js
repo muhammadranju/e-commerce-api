48f9644163c4b8a4160e1f56869d3fd909258dc1
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-02-22 11:35:23 +0600

    update routes index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,11 +6,14 @@ const authRoute = require("./auth.routes/auth.routes");
 
 const productRoute = require("./products.routes/products.routes");
 const cartsRoute = require("./carts.routes/carts.routes");
-const ordersRoute = require("./orders.routes/orders.routes");
 
+const ordersRoute = require("./orders.routes/orders.routes");
 const paymentRoute = require("./payment.route/payment.routes");
-const commentsRoute = require("./comment.routes/comment.routes");
+
 const categoriesRoute = require("./category.routes/category.routes");
+const brandRoute = require("./brand.routes/brand.routes");
+
+const commentsRoute = require("./comment.routes/comment.routes");
 const wishlistRoute = require("./wishlists.routes/wishlists.routes");
 
 router.use(ApiVersion, userRoute);
@@ -18,10 +21,14 @@ router.use(ApiVersion, authRoute);
 
 router.use(ApiVersion, productRoute);
 router.use(ApiVersion, cartsRoute);
-router.use(ApiVersion, ordersRoute);
 
-router.use(ApiVersion, paymentRoute);
-router.use(ApiVersion, commentsRoute);
 router.use(ApiVersion, categoriesRoute);
+router.use(ApiVersion, brandRoute);
+
+router.use(ApiVersion, ordersRoute);
+router.use(ApiVersion, paymentRoute);
+
+router.use(ApiVersion, commentsRoute);
 router.use(ApiVersion, wishlistRoute);
+
 module.exports = router;
