3061cee9fe98be1750327a2cf44e777483c45122
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-02-27 21:06:57 +0600

    update User.model.js

--- src/models/User.model/User.model.js
+++ src/models/User.model/User.model.js
@@ -1,5 +1,5 @@
 const { Schema, model } = require("mongoose");
-const bcrypt = require("bcrypt"); // For password hashing
+const bcrypt = require("bcryptjs"); // For password hashing
 
 const {
   ModelRefNames,
