fb0dc6e681e002bb3ce1a9251c685d5e0f384fc7
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-03-02 09:10:13 +0600

    Update auth.routes.js

--- src/routes/auth.routes/auth.routes.js
+++ src/routes/auth.routes/auth.routes.js
@@ -1,10 +1,26 @@
 const router = require("express").Router();
 const auth = require("../../controller/auth.controller/auth.controller");
 
+// this route is reset password route patch using method
+router
+  .route("/auth/reset-password/:resetToken")
+  .patch(auth.resetPasswordPostController);
+
+// this route send code on email verify token
+router
+  .route("/auth/verify-email/:verificationToken")
+  .get(auth.emailVerificationController);
+
+// register route using post method
 router.route("/auth/register").post(auth.signupPostController);
+
+// login route using post method
 router.route("/auth/login").post(auth.loginPostController);
+
+// logout route using post method
 router.route("/auth/logout").post(auth.logoutPostController);
+
+// register forgot password using post method
 router.route("/auth/forgot-password").post(auth.forgotPasswordPostController);
-router.route("/auth/reset-password").patch(auth.resetPasswordPostController);
 
 module.exports = router;
