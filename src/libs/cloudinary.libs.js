51d88e3c71d536a8c82ed65771a1556a2ac39290
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-03-12 22:23:26 +0600

    feat(comment): adding understandable comments for readability cloudinary function

--- src/libs/cloudinary.libs.js
+++ src/libs/cloudinary.libs.js
@@ -1,24 +1,35 @@
 const { v2: cloudinary } = require("cloudinary");
 const fs = require("fs");
+
+// Configure Cloudinary with API credentials
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, // Cloudinary cloud name
+  api_key: process.env.CLOUDINARY_API_KEY, // Cloudinary API key
+  api_secret: process.env.CLOUDINARY_API_SECRET, // Cloudinary API secret
 });
 
+// Function to upload a file to Cloudinary
 const uploadOnCloudinary = async (localFilePath) => {
   try {
+    // Check if localFilePath is provided
     if (!localFilePath) return null;
-    //upload the file on cloudinary
+
+    // Upload the file to Cloudinary
     const response = await cloudinary.uploader.upload(localFilePath, {
-      resource_type: "auto",
+      resource_type: "auto", // Automatically detect the resource type
     });
-    // file has been uploaded successfully
+
+    // File has been uploaded successfully
     console.log("file is uploaded on cloudinary ", response.url);
+
+    // Delete the locally saved temporary file
     fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
-    fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
+    // Remove the locally saved temporary file if the upload operation failed
+    fs.unlinkSync(localFilePath);
+
+    // Return null if upload failed
     return null;
   }
 };
