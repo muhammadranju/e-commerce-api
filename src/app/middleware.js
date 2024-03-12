9ceeb6d73489dac5d21ba6af05a352ab0f24a53b
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-03-12 22:15:07 +0600

    feat(comment): adding understandable comments for readability code and easy to understand

--- src/app/middleware.js
+++ src/app/middleware.js
@@ -1,19 +1,20 @@
 const express = require("express");
-const morgan = require("morgan");
-const compression = require("compression");
-const cookieParser = require("cookie-parser");
+const morgan = require("morgan"); // Middleware for logging HTTP requests
+const compression = require("compression"); // Middleware for compressing HTTP responses
+const cookieParser = require("cookie-parser"); // Middleware for parsing cookies
 
 // const rateLimit = require("../utils/rateLimit.utils/rateLimit.utils");
 // const userAgent = require("../middleware/userAgent.middleware");
 
 const middleware = [
-  express.json({ limit: "16kb" }),
-  express.urlencoded({ extended: true, limit: "16kb" }),
-  compression(),
-  cookieParser(),
-  // userAgent,
-  morgan("dev"),
-  // rateLimit,
+  express.json({ limit: "16kb" }), // Middleware for parsing JSON request bodies with a size limit of 16kb
+  express.urlencoded({ extended: true, limit: "16kb" }), // Middleware for parsing URL-encoded request bodies with a size limit of 16kb
+  compression(), // Middleware for compressing HTTP responses
+  cookieParser(), // Middleware for parsing cookies
+  morgan("dev"), // Morgan middleware for logging HTTP requests in the 'dev' format
+
+  // userAgent, // Custom middleware for parsing and handling user agents (commented out)
+  // rateLimit, // Custom middleware for rate limiting requests (commented out)
 ];
 
 module.exports = middleware;
--- src/config/DatabaseConnection.js
+++ src/config/DatabaseConnection.js
@@ -1,22 +1,31 @@
 const mongoose = require("mongoose");
 const { DATABASE_NAME, DATABASE_QUERY } = require("../constants");
 
+// MongoDB URI for production environment
 const MONGODB_URI = process.env.MONGODB_URI; // REPLACE WITH YOUR OWN MONGODB URI
+
+// MongoDB URI for local development environment
 const MONGODB_URI_LOCAL = process.env.MONGODB_URI_LOCAL; // REPLACE WITH YOUR LOCAL MONGODB URI
 
+// Function to establish connection with MongoDB
 async function connectDB() {
   try {
+    // Connect to the local MongoDB instance for development/testing
     const connectInstance = await mongoose.connect(
       `${MONGODB_URI_LOCAL}/${DATABASE_NAME}`
     );
 
+    // Connect to the production MongoDB instance for deployment
     // const connectInstance = await mongoose.connect(
     //   `${MONGODB_URI}/${DATABASE_NAME}${DATABASE_QUERY}`
     // );
+
+    // Log successful connection to MongoDB
     console.log(
       `MongoDB connected!! DB HOST: ${connectInstance.connection.host}`
     );
   } catch (error) {
+    // Log error and exit the process if connection fails
     console.log("MONGODB CONNECTION FIELD: ", error);
     process.exit(1);
   }
