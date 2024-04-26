b2932babe2e8ca539eeaeb0445e4ef84f134a263
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-04-26 23:26:40 +0600

    feat: update auth controller doing defining single responsibility work

--- src/routes/auth.routes/auth.routes.js
+++ src/routes/auth.routes/auth.routes.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const auth = require("../../controller/auth.controller/auth.controller");
 
+const controller = require("../../api/v1/auth/controller");
+
 const { authMiddleware } = require("../../middleware/auth.middleware");
 const isLoginMiddleware = require("../../middleware/isLogin.middleware");
 
@@ -9,7 +11,7 @@ const rateLimiter = require("../../utils/rateLimit.utils");
 // Rate limit middleware to prevent brute force attacks
 
 // this route is reset password route patch using method
-router.route("/reset-password/:resetToken").patch(auth.resetPasswordController);
+router.route("/reset-password/:resetToken").patch(controller.resetPassword);
 
 // this route send code on email verify token
 router
@@ -19,17 +21,15 @@ router
 // register route using post method
 router
   .route("/register")
-  .post(rateLimiter, isLoginMiddleware, auth.signupController);
+  .post(rateLimiter, isLoginMiddleware, controller.signup);
 
 // login route using post method
-router
-  .route("/login")
-  .post(rateLimiter, isLoginMiddleware, auth.loginController);
+router.route("/login").post(rateLimiter, isLoginMiddleware, controller.login);
 
 // logout route using post method
-router.route("/logout").post(authMiddleware, auth.logoutController);
+router.route("/logout").post(authMiddleware, controller.logout);
 
 // register forgot password using post method
-router.route("/forgot-password").post(auth.forgotPasswordController);
+router.route("/forgot-password").post(controller.forgotPassword);
 
 module.exports = router;
