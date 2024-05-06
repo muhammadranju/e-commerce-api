3f3e5a37fadb86463434760306d507efd6d23ccc
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-05-06 23:46:03 +0600

    feat: implement endpoint to find product by ID or SKU
    Add `findSingle` controller for handling requests to retrieve a product using either its ID or SKU. The function includes checks for the presence of the productId parameter and returns detailed error messages if the productId is missing or the product cannot be found. On successful product retrieval, it returns a JSON response containing the product details.

---
+++ src/api/v1/product/controller/findSingle.js
@@ -0,0 +1,38 @@
+const Product = require("../../../../models/Products.model/Products.model");
+const ApiError = require("../../../../utils/ApiError");
+const ApiResponse = require("../../../../utils/ApiResponse");
+const asyncHandler = require("../../../../utils/asyncHandler");
+
+/**
+ * Handles the HTTP request for finding a single product by its id or SKU.
+ *
+ * @param {Object} req - The HTTP request object.
+ * @param {Object} res - The HTTP response object.
+ * @returns {Promise<void>} - A Promise that resolves when the request has been handled.
+ * @throws {ApiError} - Throws an ApiError with a 400 status code if the productId is not provided.
+ * @throws {ApiError} - Throws an ApiError with a 404 status code if the product is not found.
+ */
+const findSingle = asyncHandler(async (req, res) => {
+  // Get the productId from the request parameters
+  const { productId } = req.params;
+
+  // Check if the productId is provided
+  if (!productId) {
+    throw new ApiError(400, "productId is required");
+  }
+
+  // Find the product in the database using the productId or SKU
+  const product = await Product.findOne({
+    $or: [{ slug: productId }, { SKU: productId }],
+  });
+
+  // Check if the product is found in the database
+  if (!product) {
+    throw new ApiError(404, "Product not found");
+  }
+
+  // Return the product as a JSON response with a 200 status code (OK) and a success message
+  return res.json(new ApiResponse(200, product, "Product found"));
+});
+
+module.exports = findSingle;
