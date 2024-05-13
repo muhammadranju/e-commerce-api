c354f3552f9eeb1a7da0a240a3da6997d3ed103d
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-05-14 00:04:08 +0600

    feat(order): Implement findSingleOrderController to retrieve a single order
    Added `findSingleOrderController` to fetch a single order from the database by its ID. The controller ensures that a valid orderId is provided and retrieves the order details, including user and items information. It uses mongoose's populate method to include user details and populate product details for each item in the order

---
+++ src/api/v1/order/controller/findSingle.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+const Order = require("../../../../models/Orders.model/Orders.model");
+const ApiError = require("../../../../utils/ApiError");
+const asyncHandler = require("../../../../utils/asyncHandler");
+const ApiResponse = require("../../../../utils/ApiResponse");
+
+/**
+ * Asynchronously retrieves a single order from the database by its ID.
+ *
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @return {Promise<Object>} The JSON response containing the retrieved order.
+ */
+const findSingleOrderController = asyncHandler(async (req, res) => {
+  const { orderId } = req.params;
+
+  // Check if orderId is provided
+  if (!orderId) {
+    throw new ApiError(400, "Order ID is required.");
+  }
+
+  // Check if orderId is valid
+  if (!mongoose.Types.ObjectId.isValid(orderId)) {
+    throw new ApiError(
+      400,
+      "Invalid order id. Please provide a valid order id."
+    );
+  }
+
+  // Find the order by orderId and populate user and items details
+  const order = await Order.findById(orderId)
+    .populate("user") // Populate user details
+    .populate({
+      // Populate items details including product details
+      path: "items",
+      populate: {
+        path: "product",
+        model: "Product",
+      },
+    });
+
+  if (!order) {
+    throw new ApiError(404, "Order not found");
+  }
+
+  // Return the retrieved order in the response
+  return res
+    .status(200)
+    .json(new ApiResponse(200, order, "Order retrieved successfully"));
+});
+
+module.exports = findSingleOrderController;
