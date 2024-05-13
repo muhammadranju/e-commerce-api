05358f2ff95a0558ba8e3242d48245431d49e612
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-05-14 00:05:29 +0600

    feat(order): Implement deleteOrderController to delete orders
    Added `deleteOrderController` to allow users to delete their orders. The controller ensures that the orderId is provided and valid, throwing appropriate errors if not. It then retrieves the order based on the orderId and checks if the user is authorized to delete it.

---
+++ src/api/v1/order/controller/delete.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+const Order = require("../../../../models/Orders.model/Orders.model");
+const ApiError = require("../../../../utils/ApiError");
+const ApiResponse = require("../../../../utils/ApiResponse");
+const asyncHandler = require("../../../../utils/asyncHandler");
+
+/**
+ * Asynchronously deletes an order from the database.
+ *
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @return {Promise<Object>} The JSON response indicating the success of deletion.
+ */
+const deleteOrderController = asyncHandler(async (req, res) => {
+  const { orderId } = req.params;
+  const userId = req.user.userId;
+
+  // Check if orderId is provided
+  if (!orderId) {
+    throw new ApiError(400, "Order ID is required.");
+  }
+
+  // Check if orderId is valid
+  if (!mongoose.Types.ObjectId.isValid(orderId)) {
+    throw new ApiError(
+      400,
+      "Invalid order id. Please provide a valid order id."
+    );
+  }
+
+  // Find the order by orderId
+  const order = await Order.findById(orderId);
+
+  if (!order) {
+    throw new ApiError(404, "Order not found");
+  }
+
+  // Check if the user is authorized to delete the order
+  if (order.user.toString() !== userId) {
+    throw new ApiError(403, "Unauthorized to delete order");
+  }
+
+  // Delete the order from the database
+  await order.deleteOne();
+
+  // Return the response indicating successful deletion
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        204,
+        { deleted: "204 No Content", orderId: order._id },
+        "Order deleted successfully"
+      )
+    );
+});
+
+module.exports = deleteOrderController;
---
+++ src/api/v1/order/controller/index.js
@@ -0,0 +1,13 @@
+const create = require("./create");
+const findSingle = require("./findSingle");
+const findAll = require("./findAll");
+const update = require("./update");
+const deleteItem = require("./delete");
+
+module.exports = {
+  create,
+  findSingle,
+  findAll,
+  update,
+  deleteItem,
+};
