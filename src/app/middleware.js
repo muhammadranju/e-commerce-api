ceee18b9c2e4df80a47cbd017a118c64988d0431
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-05-16 00:03:24 +0600

    feat: add middleware to extract and store host information from request headers
    This middleware, named getHost, constructs the host URL using the request's protocol and host headers. It attaches the host URL to the request object as req.myHost for convenient access in subsequent middleware or route handlers.

--- src/app/middleware.js
+++ src/app/middleware.js
@@ -2,8 +2,10 @@ const express = require("express");
 const morgan = require("morgan"); // Middleware for logging HTTP requests
 const compression = require("compression"); // Middleware for compressing HTTP responses
 const cookieParser = require("cookie-parser"); // Middleware for parsing cookies
-const helmet = require("helmet");
+const helmet = require("helmet"); // Middleware for setting security-related HTTP headers
+const cors = require("cors"); // Middleware for providing a Connect/Express middleware that can be used to enable CORS with various options
 
+const host = require("../middleware/getHost");
 // const rateLimit = require("../utils/rateLimit.utils/rateLimit.utils");
 // const userAgent = require("../middleware/userAgent.middleware");
 
@@ -14,7 +16,8 @@ const middleware = [
   cookieParser(), // Middleware for parsing cookies
   morgan("dev"), // Morgan middleware for logging HTTP requests in the 'dev' format
   helmet(), // Helmet middleware for setting security-related HTTP headers
-
+  cors(), // CORS middleware for handling cross-origin resource sharing
+  host,
   // userAgent, // Custom middleware for parsing and handling user agents (commented out)
   // rateLimit, // Custom middleware for rate limiting requests (commented out)
 ];
