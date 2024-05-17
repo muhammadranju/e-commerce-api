6d795de278738f2656f25a696b2beafab7a81571
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-05-18 00:50:35 +0600

    feat: Added HATEOAS links to the changePasswordController response for better API discoverability and navigation.

--- src/api/v1/auth/controller/changePassword.js
+++ src/api/v1/auth/controller/changePassword.js
@@ -38,14 +38,31 @@ const changePasswordController = asyncHandler(async (req, res) => {
   user.password = newPassword;
   await user.save(); // save to database
 
+  const host = req.apiHost;
+  // HATEOAS links
+  const links = [
+    {
+      rel: "logout",
+      href: `${host}/auth/logout`,
+      method: "POST",
+      description: "Logout",
+    },
+    {
+      rel: "reset_password",
+      href: `${host}/auth/reset-password`,
+      method: "PATCH",
+      description: "Reset Password",
+    },
+    // Add more links as needed
+  ];
   // Respond with success message
-
   return res
     .status(200)
     .json(
       new ApiResponse(
         200,
         { userId: user._id },
+        links,
         "Password was change successfully."
       )
     );
