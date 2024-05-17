317efddbd4e353f5dda7e389f83470c7b0372215
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-05-17 21:17:29 +0600

    feat: Updated the getHost middleware to also attach the API version with the host URL for easy access in subsequent middleware or route handlers.

--- src/middleware/getHost.js
+++ src/middleware/getHost.js
@@ -1,9 +1,18 @@
+const { ApiVersion } = require("../constants");
+
 // Middleware to extract and store the host information from the request
 const getHost = (req, _res, next) => {
   // Construct the host URL using the request's protocol and host headers
   const host = `${req.protocol}://${req.get("host")}`;
+
+  // Attach the host URL to the request object
+  const ApiVersionWithHost = `${req.protocol}://${req.get(
+    "host"
+  )}${ApiVersion}`;
+
   // Attach the host URL to the request object for easy access in subsequent middleware or route handlers
   req.myHost = host;
+  req.apiHost = ApiVersionWithHost;
   // Move to the next middleware in the chain
   next();
 };
