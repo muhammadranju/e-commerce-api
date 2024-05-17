ca5738bf1790ede546ab2c454ef18e347412cba5
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-05-17 20:56:48 +0600

    feat: Enhanced retrieveAddressController with HATEOAS links:
    - Added API versioning and HATEOAS links to the response.
    - Included links for creating, updating, and deleting addresses.
    - Ensured response structure includes addresses and links.

--- src/api/v1/address/controller/findAll.js
+++ src/api/v1/address/controller/findAll.js
@@ -1,3 +1,4 @@
+const { ApiVersion } = require("../../../../constants");
 const Address = require("../../../../models/Address.model/Address.model");
 const ApiResponse = require("../../../../utils/ApiResponse");
 const asyncHandler = require("../../../../utils/asyncHandler");
@@ -5,11 +6,35 @@ const asyncHandler = require("../../../../utils/asyncHandler");
 const retrieveAddressController = asyncHandler(async (req, res) => {
   // Gating the addresses in to database
   const address = await Address.find();
+  const host = `${req.myHost}${ApiVersion}`;
+  // Creating HATEOAS links for each address
+  const links = [
+    {
+      rel: "create",
+      href: `${host}/user/profile/address`,
+      method: "POST",
+      description: "Create Addresses",
+    },
+    {
+      rel: "update",
+      href: `${host}/user/profile/address`,
+      method: "PUT",
+      description: "Update Addresses",
+    },
+    {
+      rel: "delete",
+      href: `${host}/user/profile/address`,
+      method: "DELETE",
+      description: "Delete Addresses",
+    },
+  ];
 
   // Response the addresses data
   return res
     .status(200)
-    .json(new ApiResponse(200, { address }, "Address grt successfully."));
+    .json(
+      new ApiResponse(200, { address, links }, "Address grt successfully.")
+    );
 });
 
 module.exports = retrieveAddressController;
