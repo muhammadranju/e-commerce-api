4492c29ce9b3ad56780436a6aaef478c8bd1c6b7
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-05-17 20:59:20 +0600

    fix: Removed findSingle controller from module exports to streamline the API.

--- src/api/v1/address/controller/findSingle.js
+++
@@ -1,5 +0,0 @@
-const asyncHandler = require("../../../../utils/asyncHandler");
-
-const singleAddressController = asyncHandler(async (req, res, next) => {});
-
-module.exports = singleAddressController;
--- src/api/v1/address/controller/index.js
+++ src/api/v1/address/controller/index.js
@@ -1,7 +1,6 @@
 const create = require("./create");
 const findAll = require("./findAll");
-const findSingle = require("./findSingle");
 const update = require("./update");
 const deleteAddress = require("./delete");
 
-module.exports = { create, findAll, findSingle, update, deleteAddress };
+module.exports = { create, findAll, update, deleteAddress };
