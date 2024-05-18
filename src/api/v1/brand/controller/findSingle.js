8afc9ce73b9ad5ce5fdb8a5ce252c1e6b309874d
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-05-19 00:33:00 +0600

    feat: Add HATEOAS links to the brandByIdController response for enhanced API navigation

--- src/api/v1/brand/controller/findSingle.js
+++ src/api/v1/brand/controller/findSingle.js
@@ -3,7 +3,7 @@ const ApiError = require("../../../../utils/ApiError");
 const ApiResponse = require("../../../../utils/ApiResponse");
 const asyncHandler = require("../../../../utils/asyncHandler");
 
-const brandByIdController = asyncHandler(async (req, res, next) => {
+const brandByIdController = asyncHandler(async (req, res) => {
   // Get brand URL from params
   const brandUrl = req.params?.brandId;
 
@@ -15,10 +15,42 @@ const brandByIdController = asyncHandler(async (req, res, next) => {
     throw new ApiError(404, "This Brand is not found.");
   }
 
+  const host = req.apiHost;
+
+  // HATEOAS links
+  const links = [
+    {
+      rel: "self",
+      href: `${host}/brands/${brandUrl}`,
+      method: "GET",
+      description: "Get details of this brand",
+    },
+    {
+      rel: "all_brands",
+      href: `${host}/brands`,
+      method: "GET",
+      description: "Get all brands",
+    },
+    {
+      rel: "update_brand",
+      href: `${host}/brands/${brandUrl}`,
+      method: "PUT",
+      description: "Update this brand",
+    },
+    {
+      rel: "delete_brand",
+      href: `${host}/brands/${brandUrl}`,
+      method: "DELETE",
+      description: "Delete this brand",
+    },
+  ];
+
   // If brand exists, send the brand details in the response
   return res
     .status(200)
-    .json(new ApiResponse(200, { brand }, "Fetch single brand successfully."));
+    .json(
+      new ApiResponse(200, { brand, links }, "Fetch single brand successfully.")
+    );
 });
 
 module.exports = brandByIdController;
