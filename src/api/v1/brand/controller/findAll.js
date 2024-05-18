afc127dba250fee095bcf680d2c82a7fb83875c0
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-05-19 00:32:32 +0600

    feat: Add HATEOAS links to the brandsGetController response for enhanced API navigation

--- src/api/v1/brand/controller/findAll.js
+++ src/api/v1/brand/controller/findAll.js
@@ -3,7 +3,7 @@ const ApiError = require("../../../../utils/ApiError");
 const ApiResponse = require("../../../../utils/ApiResponse");
 const asyncHandler = require("../../../../utils/asyncHandler");
 
-const brandsGetController = asyncHandler(async (req, res, next) => {
+const brandsGetController = asyncHandler(async (req, res) => {
   // Retrieve all brands with their details
   const brands = await Brand.find();
 
@@ -12,10 +12,29 @@ const brandsGetController = asyncHandler(async (req, res, next) => {
     throw new ApiError(404, "Brands not available yet.");
   }
 
+  const host = req.apiHost;
+  // HATEOAS links
+  const links = [
+    {
+      rel: "self",
+      href: `${host}/brands`,
+      method: "GET",
+      description: "Get all brands",
+    },
+    {
+      rel: "create_brand",
+      href: `${host}/brands`,
+      method: "POST",
+      description: "Create a new brand",
+    },
+  ];
+
   // If brands are available, return them in JSON format
   return res
     .status(200)
-    .json(new ApiResponse(200, { brands }, "Fetch brands successfully."));
+    .json(
+      new ApiResponse(200, { brands, links }, "Fetch brands successfully.")
+    );
 });
 
 module.exports = brandsGetController;
