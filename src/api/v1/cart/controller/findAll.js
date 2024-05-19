7521a6f682af1c60e8b444bd9ad6682de91631ee
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-05-20 00:25:54 +0600

    feat: Add HATEOAS links to the findAddTOCartController response for enhanced API navigation

--- src/api/v1/cart/controller/findAll.js
+++ src/api/v1/cart/controller/findAll.js
@@ -18,10 +18,39 @@ const findAddTOCartController = asyncHandler(async (req, res) => {
     throw new ApiError(404, "Cart not found for this user");
   }
 
+  const host = req.apiHost;
+  // HATEOAS links
+  const links = [
+    {
+      rel: "self",
+      href: `${host}/carts`,
+      method: "GET",
+      description: "View your cart",
+    },
+    {
+      rel: "add_to_cart",
+      href: `${host}/carts`,
+      method: "POST",
+      description: "Add item to cart",
+    },
+    {
+      rel: "empty_cart",
+      href: `${host}/carts/empty`,
+      method: "DELETE",
+      description: "Empty the cart",
+    },
+    {
+      rel: "remove_item",
+      href: `${host}/carts/`,
+      method: "DELETE",
+      description: "Remove an item from the cart",
+    },
+  ];
+
   // Return the cart to the user (including populated product details)
   return res
     .status(200)
-    .json(new ApiResponse(200, cart, "Cart fetched successfully"));
+    .json(new ApiResponse(200, { cart, links }, "Cart fetched successfully"));
 });
 
 module.exports = findAddTOCartController;
