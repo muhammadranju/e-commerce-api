1152465846a29298f4c34ed9088396005576bab9
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-05-20 00:28:49 +0600

    feat: Update route for deleting all items in cart to use "/empty" endpoint instead of "/"

--- src/routes/carts.routes/carts.routes.js
+++ src/routes/carts.routes/carts.routes.js
@@ -8,6 +8,6 @@ router.route("/").get(authMiddleware, cart.findAll);
 router.route("/").post(authMiddleware, cart.create);
 router.route("/:productId").patch(authMiddleware, cart.update);
 router.route("/:productId").delete(authMiddleware, cart.deleteItem);
-router.route("/").delete(authMiddleware, cart.deleteAll);
+router.route("/empty").delete(authMiddleware, cart.deleteAll);
 
 module.exports = router;
