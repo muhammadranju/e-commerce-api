c86b420ab2e6ec488948c6a7f3f1e91e0e953980
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-05-21 09:30:24 +0600

    feat: Add HATEOAS links to the response in the category retrieval controller.

--- src/api/v1/category/controller/findAll.js
+++ src/api/v1/category/controller/findAll.js
@@ -15,11 +15,41 @@ const categoriesGetController = asyncHandler(async (req, res) => {
     throw new ApiError(404, "There are not found.");
   }
 
+  const host = req.apiHost;
+  // Create HATEOAS links for each category
+  const categoriesWithLinks = categories.map((category) => ({
+    ...category._doc,
+    links: [
+      {
+        rel: "self",
+        href: `${host}/categories/${category?.category_url}`,
+        method: "GET",
+        description: "Retrieve this category",
+      },
+      {
+        rel: "update_category",
+        href: `${host}/categories/${category?.category_url}`,
+        method: "PUT",
+        description: "Update this category",
+      },
+      {
+        rel: "delete_category",
+        href: `${host}/categories/${category?.category_url}`,
+        method: "DELETE",
+        description: "Delete this category",
+      },
+    ],
+  }));
+
   // return as a json formate
   return res
     .status(200)
     .json(
-      new ApiResponse(200, { categories }, "Get all categories successfully.")
+      new ApiResponse(
+        200,
+        { categoriesWithLinks },
+        "Get all categories successfully."
+      )
     );
 });
 module.exports = categoriesGetController;
