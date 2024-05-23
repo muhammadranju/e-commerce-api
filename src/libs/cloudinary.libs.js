215eef9c4e46e51936e8cecd6d5ce85604b467b0
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-05-23 09:42:18 +0600

    feat: Refactor to use configuration constants from `config.js` for Cloudinary and token secrets.

--- src/libs/cloudinary.libs.js
+++ src/libs/cloudinary.libs.js
@@ -1,11 +1,11 @@
 const { v2: cloudinary } = require("cloudinary");
 const fs = require("fs");
-
+const config = require("../config/config");
 // Configure Cloudinary with API credentials
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, // Cloudinary cloud name
-  api_key: process.env.CLOUDINARY_API_KEY, // Cloudinary API key
-  api_secret: process.env.CLOUDINARY_API_SECRET, // Cloudinary API secret
+  cloud_name: config.CLOUDINARY_CLOUD_NAME, // Cloudinary cloud name
+  api_key: config.CLOUDINARY_API_KEY, // Cloudinary API key
+  api_secret: config.CLOUDINARY_API_SECRET, // Cloudinary API secret
 });
 
 // Function to upload a file to Cloudinary
@@ -27,6 +27,8 @@ const uploadOnCloudinary = async (localFilePath) => {
     return response;
   } catch (error) {
     // Remove the locally saved temporary file if the upload operation failed
+    console.log(error);
+    
     fs.unlinkSync(localFilePath);
 
     // Return null if upload failed
