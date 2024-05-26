181a3a12cff04b61407e4305f0a90289c90ec585
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-05-26 22:23:43 +0600

    feat: Enhance findSingle with HATEOAS links and structured response
    - Added HATEOAS links to the findSingle response for better API navigation.
    - Included links for self, update-store, and delete-store actions.
    - Structured the response to include HATEOAS links alongside the found store data.
    - Maintained existing functionality for finding the store using storeUID or storeURI.
    - Ensured consistent API response with status code and success message.

--- src/api/v1/seller/store/controller/findSingle.js
+++ src/api/v1/seller/store/controller/findSingle.js
@@ -17,10 +17,46 @@ const findSingle = asyncHandler(async (req, res) => {
     throw new ApiError(404, "Store not found");
   }
 
+  const host = req.apiHost;
+
+  // HATEOAS links
+  const links = [
+    {
+      rel: "self",
+      href: [
+        `${host}/seller/stores/${findStore.storeURI}`,
+        `${host}/seller/stores/${findStore.storeUID}`,
+      ],
+      method: "GET",
+      description: "Get details of the found store",
+    },
+    {
+      rel: "update-store",
+      href: [
+        `${host}/seller/stores/${findStore.storeURI}`,
+        `${host}/seller/stores/${findStore.storeUID}`,
+      ],
+
+      method: "PUT",
+      description: "Update this store",
+    },
+    {
+      rel: "delete-store",
+      href: [
+        `${host}/seller/stores/${findStore.storeURI}`,
+        `${host}/seller/stores/${findStore.storeUID}`,
+      ],
+      method: "DELETE",
+      description: "Delete this store",
+    },
+  ];
+
   // Return the store as a JSON response with a 200 status code (OK) and a success message
   return res
     .status(200)
-    .json(new ApiResponse(200, { findStore }, "Store found successfully"));
+    .json(
+      new ApiResponse(200, { findStore, links }, "Store found successfully")
+    );
 });
 
 module.exports = findSingle;
