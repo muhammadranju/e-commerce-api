0fa750af16b3d4909d4af3be33e6d2a69f6eb782
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-05-26 22:24:58 +0600

    feat: Refactor store routes for consistency and clarity
    - Renamed the endpoint "/store" to "/stores" for consistency with other routes.
    - Updated route definitions to match the modified endpoint names.
    - Ensured consistent naming convention throughout the routes.

--- src/routes/store.routes/store.routes.js
+++ src/routes/store.routes/store.routes.js
@@ -2,9 +2,9 @@ const router = require("express").Router();
 const { store } = require("../../api/v1/seller");
 const { sellerAuthMiddleware } = require("../../middleware/auth.middleware");
 
-router.route("/store").post(sellerAuthMiddleware, store.create);
-router.route("/store/:storeId").get(store.findSingle);
-router.route("/store/:storeId").patch(sellerAuthMiddleware, store.update);
-router.route("/store/:storeId").delete(sellerAuthMiddleware, store.deleteShop);
+router.route("/stores").post(sellerAuthMiddleware, store.create);
+router.route("/stores/:storeId").get(store.findSingle);
+router.route("/stores/:storeId").patch(sellerAuthMiddleware, store.update);
+router.route("/stores/:storeId").delete(sellerAuthMiddleware, store.deleteShop);
 
 module.exports = router;
