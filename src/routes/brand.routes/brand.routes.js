e99e8f7a24174bbb118ec98e27cfa7f51fa42874
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-05-29 00:43:24 +0600

    feat: refactor(router): Update brand routes to use new restrictedMode middleware
    - Removed `restrictedArray` middleware and its corresponding comment.
    - Integrated `canPerform` and `setAbilities` middleware for access control.
    - Enhanced route definitions for POST, PATCH, and DELETE operations with the new middleware.
    - Ensured brand routes maintain appropriate access control and authorization checks.

--- src/routes/brand.routes/brand.routes.js
+++ src/routes/brand.routes/brand.routes.js
@@ -1,25 +1,33 @@
 const router = require("express").Router();
 const { controller: brand } = require("../../api/v1/brand");
-const { UserRolesEnum } = require("../../constants");
-const restricted = require("../../middleware/restricted.middleware");
 const {
   sellerAuthMiddleware: adminAuth,
 } = require("../../middleware/auth.middleware");
+
+const { canPerform, setAbilities } = require("../../middleware/restrictedMode");
+
 /**
  * Restricts access to the specified user roles.
  * @param {...UserRolesEnum} roles - The user roles to restrict access for.
  * @returns {Array<string>} An array of restricted user roles.
  */
-const restrictedArray = restricted(
-  UserRolesEnum.ADMIN,
-  UserRolesEnum.EDITOR,
-  UserRolesEnum.MANAGER
-);
 
 router.route("/").get(brand.findAll);
-router.route("/").post(adminAuth, restrictedArray, brand.create);
 router.route("/:brandId").get(brand.findSingle);
-router.route("/:brandId").patch(adminAuth, restrictedArray, brand.update);
-router.route("/:brandId").delete(adminAuth, restrictedArray, brand.deleteBrand);
+
+router
+  .route("/")
+  .post(adminAuth, setAbilities, canPerform("create", "Brands"), brand.create);
+router
+  .route("/:brandId")
+  .patch(adminAuth, setAbilities, canPerform("update", "Brands"), brand.update);
+router
+  .route("/:brandId")
+  .delete(
+    adminAuth,
+    setAbilities,
+    canPerform("delete", "Brands"),
+    brand.deleteBrand
+  );
 
 module.exports = router;
