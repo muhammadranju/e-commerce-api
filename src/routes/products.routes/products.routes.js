26147aa894ecb5cc73318e94748040f347603c72
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-06-01 01:09:04 +0600

    feat: Refactor Product Routes to Enhance Role-Based Access Control
    - Replaced restricted middleware with new restrictedMode middleware functions.
    - Added setAbilities and canPerform middleware to validate user permissions for product creation, update, and deletion.
    - Simplified import statements for middleware.
    - Ensured consistency in route definitions and middleware application.

--- src/routes/products.routes/products.routes.js
+++ src/routes/products.routes/products.routes.js
@@ -1,23 +1,38 @@
 const router = require("express").Router();
 const { controller: product } = require("../../api/v1/product/");
-const { UserRolesEnum } = require("../../constants");
-const { sellerAuthMiddleware } = require("../../middleware/auth.middleware");
+const {
+  sellerAuthMiddleware: sellerAuth,
+} = require("../../middleware/auth.middleware");
 // Define validation and sanitization rules
-const restricted = require("../../middleware/restricted.middleware");
-const restrictedArray = restricted(
-  UserRolesEnum.ADMIN,
-  UserRolesEnum.EDITOR,
-  UserRolesEnum.MANAGER,
-  UserRolesEnum.SELLER
-);
+const { setAbilities, canPerform } = require("../../middleware/restrictedMode");
+
 router.route("/").get(product.findAll);
-router.route("/").post(sellerAuthMiddleware, restrictedArray, product.create);
 router.route("/:productId").get(product.findSingle);
+
+router
+  .route("/")
+  .post(
+    sellerAuth,
+    setAbilities,
+    canPerform("create", "Products"),
+    product.create
+  );
+
 router
   .route("/:productId")
-  .patch(sellerAuthMiddleware, restrictedArray, product.update);
+  .patch(
+    sellerAuth,
+    setAbilities,
+    canPerform("update", "Products"),
+    product.update
+  );
 router
   .route("/:productId")
-  .delete(sellerAuthMiddleware, restrictedArray, product.deleteProduct);
+  .delete(
+    sellerAuth,
+    setAbilities,
+    canPerform("delete", "Products"),
+    product.deleteProduct
+  );
 
 module.exports = router;
