15eed37d82248d3df3df11bc47a09d8874eaefe9
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-05-31 19:46:13 +0600

    feat: Enhance category retrieval to include parent category details and improve response format
    - Added `.populate("parent")` to include parent category details in the response
    - Modified response to include parent category information: name, URL, description, status, and image
    - Refactored the response structure to provide a more detailed category object
    - Updated HATEOAS links to ensure proper navigation for category operations
    - Ensured consistent and informative API responses for category retrieval by ID

--- src/api/v1/category/controller/findSingle.js
+++ src/api/v1/category/controller/findSingle.js
@@ -10,17 +10,36 @@ const categoriesGetByIdController = asyncHandler(async (req, res) => {
   // check the category id from database is exits or not
   const category = await Category.findOne({
     category_url: categoryId,
-  })?.select("name description image isActive category_url");
+  })
+    ?.select("name description image isActive category_url")
+    .populate("parent");
 
   // if not exits on database then return error "This category is not found."
   if (!category) {
     throw new ApiError(404, "This Category not found.");
   }
 
+  // if exits on database then return category data
+  const categories = {
+    name: category.name,
+    description: category.description,
+    parentCategory: {
+      name: category?.parent?.name,
+      category_url: category?.parent?.category_url,
+      description: category?.parent?.description,
+      isActive: category?.parent?.isActive,
+      image: category?.parent?.image,
+    },
+    category_url: category.category_url,
+    isActive: category.isActive,
+    image: category.image,
+  };
+
   const host = req.apiHost;
   // Add HATEOAS links to the category
   const categoryWithLinks = {
-    ...category._doc,
+    // ...category._doc,
+    ...categories,
     links: [
       {
         rel: "self",
@@ -42,16 +61,16 @@ const categoriesGetByIdController = asyncHandler(async (req, res) => {
       },
     ],
   };
+
   // if it's have on database return as a json formate
-  return res
-    .status(200)
-    .json(
-      new ApiResponse(
-        200,
-        { categories: categoryWithLinks },
-        "Fetched the category successfully."
-      )
-    );
+  return res.status(200).json(
+    new ApiResponse(
+      200,
+      { categories: categoryWithLinks },
+      // { categories },
+      "Fetched the category successfully."
+    )
+  );
 });
 
 module.exports = categoriesGetByIdController;
