b86dfc723c5f545bc7289e2f901e104b353ad5af
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-05-31 21:38:31 +0600

    feat: Enhance restricted Middleware for Improved Role-Based Access Control
    - Added validation to ensure at least one role is provided to the restricted middleware.
    - Included checks for both user and seller roles in the middleware.
    - Added console logging for debugging user and seller role checks.
    - Refined error handling for unauthorized access and missing roles.

--- src/middleware/restricted.middleware.js
+++ src/middleware/restricted.middleware.js
@@ -1,11 +1,24 @@
 const ApiError = require("../utils/ApiError");
 
 const restricted = (...roles) => {
+  // If no roles are provided, throw an error
+  if (roles.length === 0) {
+    throw new ApiError(401, "Must provide at least one role");
+  }
   return (req, res, next) => {
-    if (!roles.includes(req.user.role)) {
-      throw new ApiError("You don't have permission to access", 403);
+    // If the user is not authenticated, throw an error
+    console.log(req.seller);
+    console.log(roles);
+
+    // if (!req.user || !req.seller) {
+    //   throw new ApiError(401, "Unauthorized access to this resource");
+    // }
+    // If the user is authenticated, but is not an admin, throw an error
+    if (roles.includes(req.user.role) || roles.includes(req.seller.role)) {
+      throw new ApiError(403, "You don't have permission to access");
     }
-    next();
+    // If the user is authenticated and is an admin, continue with the request
+    return next();
   };
 };
 
