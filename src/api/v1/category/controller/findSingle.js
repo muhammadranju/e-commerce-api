e7248ade0a9673554c79926acebcffe446e3326c
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-06-06 11:29:35 +0600

    feat: Refactored categoriesGetByIdController: improved readability and documentation, streamlined category data fetching and HATEOAS link generation, and ensured consistent response format.

--- src/api/v1/category/controller/findSingle.js
+++ src/api/v1/category/controller/findSingle.js
@@ -3,74 +3,68 @@ const ApiError = require("../../../../utils/ApiError");
 const ApiResponse = require("../../../../utils/ApiResponse");
 const asyncHandler = require("../../../../utils/asyncHandler");
 
+/**
+ * Controller to get a category by its ID.
+ *
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @return {Promise<Object>} The JSON response containing the category.
+ */
 const categoriesGetByIdController = asyncHandler(async (req, res) => {
-  // get category by id come form params categoryId
+  // Extract categoryId from request parameters
   const { categoryId } = req.params;
 
-  // check the category id from database is exits or not
-  const category = await Category.findOne({
-    category_url: categoryId,
-  })
-    ?.select("name description image isActive category_url")
-    .populate("parent");
+  // Fields to select from the Category document
+  const selectFields = "name description image isActive category_url";
 
-  // if not exits on database then return error "This category is not found."
+  // Find the category by its URL, select specified fields, and populate parent and subParent fields
+  const category = await Category.findOne({ category_url: categoryId })
+    .select(selectFields)
+    .populate("parent", selectFields);
+
+  // If the category is not found, throw a 404 error
   if (!category) {
     throw new ApiError(404, "This Category not found.");
   }
 
-  // if exits on database then return category data
-  const categories = {
-    name: category.name,
-    description: category.description,
-    parentCategory: {
-      name: category?.parent?.name,
-      category_url: category?.parent?.category_url,
-      description: category?.parent?.description,
-      isActive: category?.parent?.isActive,
-      image: category?.parent?.image,
-    },
-    category_url: category.category_url,
-    isActive: category.isActive,
-    image: category.image,
-  };
-
+  // Define the host for HATEOAS links
   const host = req.apiHost;
-  // Add HATEOAS links to the category
+
+  // Add HATEOAS links to the category object
   const categoryWithLinks = {
-    // ...category._doc,
-    ...categories,
+    ...category._doc,
     links: [
       {
         rel: "self",
-        href: `${host}/categories/${category?.category_url}`,
+        href: `${host}/categories/${category.category_url}`,
         method: "GET",
         description: "Retrieve this category",
       },
       {
         rel: "update_category",
-        href: `${host}/categories/${category?.category_url}`,
+        href: `${host}/categories/${category.category_url}`,
         method: "PUT",
         description: "Update this category",
       },
       {
         rel: "delete_category",
-        href: `${host}/categories/${category?.category_url}`,
+        href: `${host}/categories/${category.category_url}`,
         method: "DELETE",
         description: "Delete this category",
       },
     ],
   };
 
-  // if it's have on database return as a json formate
-  return res.status(200).json(
-    new ApiResponse(
-      200,
-      { categories: categoryWithLinks },
-      // { categories },
-      "Fetched the category successfully."
-    )
-  );
+  // Return the category with links as a JSON response
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { category: categoryWithLinks },
+        "Fetched the category successfully."
+      )
+    );
 });
 
 module.exports = categoriesGetByIdController;
