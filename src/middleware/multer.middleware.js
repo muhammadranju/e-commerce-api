910a35ca5edb3c3099a5413ec523648ac5873eab
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-06-07 17:10:26 +0600

    chore!: Enhance multer file upload handling with unique filenames
    - Integrated the `path` module to manage file extensions.
    - Modified the filename function to generate unique filenames by appending a timestamp.
    - Replaced spaces in filenames with hyphens and converted to lowercase for consistency.
    - Improved handling to avoid filename collisions and ensure better file organization.

--- src/middleware/multer.middleware.js
+++ src/middleware/multer.middleware.js
@@ -1,11 +1,24 @@
 const multer = require("multer");
+const path = require("path");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "./public/temp");
   },
+
   filename: function (req, file, cb) {
-    cb(null, file.originalname);
+    // cb(null, file.originalname);
+    const fileExtensionName = path.extname(file.originalname);
+    const fileName =
+      file.originalname
+        .replace(fileExtensionName, "")
+        .toLowerCase()
+        .split(" ")
+        .join("-") +
+      "-" +
+      Date.now() +
+      fileExtensionName;
+    cb(null, fileName);
   },
 });
 
