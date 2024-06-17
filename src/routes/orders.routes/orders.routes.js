7e18848a323455462aa89ca6d4fa256e8b03e3f3
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-06-17 11:57:30 +0600

    feat(orders.routes)!: Refactor order routes to use dynamic permissions middleware
    - Replaced `restrictedArray` with dynamic permission checks using `setAbilities` and `canPerform`.
    - Enhanced route security by specifying required actions (create, read, update, delete) on the "Orders" resource.
    - Improved code readability and maintainability by centralizing permission logic.

--- src/routes/orders.routes/orders.routes.js
+++ src/routes/orders.routes/orders.routes.js
@@ -1,26 +1,48 @@
 const router = require("express").Router();
 const { authMiddleware } = require("../../middleware/auth.middleware");
 const { controller: order } = require("../../api/v1/order");
-const restricted = require("../../middleware/restricted.middleware");
-const { UserRolesEnum } = require("../../constants");
-const restrictedArray = restricted(
-  UserRolesEnum.ADMIN,
-  UserRolesEnum.EDITOR,
-  UserRolesEnum.MANAGER,
-  UserRolesEnum.USER
-);
+const { canPerform, setAbilities } = require("../../middleware/restrictedMode");
 
-router.route("/orders/").post(authMiddleware, restrictedArray, order.create);
+router
+  .route("/orders/")
+  .post(
+    authMiddleware,
+    setAbilities,
+    canPerform("create", "Orders"),
+    order.create
+  );
 router
   .route("/orders/:orderId")
-  .get(authMiddleware, restrictedArray, order.findSingle);
+  .get(
+    authMiddleware,
+    setAbilities,
+    canPerform("read", "Orders"),
+    order.findSingle
+  );
 router
   .route("/orders/:orderId")
-  .patch(authMiddleware, restrictedArray, order.update);
+  .patch(
+    authMiddleware,
+    setAbilities,
+    canPerform("update", "Orders"),
+    order.update
+  );
 
 router
   .route("/orders/:orderId/confirm")
-  .delete(authMiddleware, restrictedArray, order.deleteItem);
-router.route("/orders").get(authMiddleware, restrictedArray, order.findAll);
+  .delete(
+    authMiddleware,
+    setAbilities,
+    canPerform("delete", "Orders"),
+    order.deleteItem
+  );
+router
+  .route("/orders")
+  .get(
+    authMiddleware,
+    setAbilities,
+    canPerform("read", "Orders"),
+    order.findAll
+  );
 
 module.exports = router;
