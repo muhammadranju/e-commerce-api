080d88793b606b5c851dda99378f5dd62448f1a0
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-06-24 01:09:16 +0600

    feat: Add IPN payment controller for handling asynchronous payment notifications
    - Implemented `ipnPaymentController` to process IPN (Instant Payment Notification) from SSLCommerz.
    - Extracted necessary payment details from the request body.
    - Validated transaction ID and ensured the associated order exists.
    - Checked for duplicate payment status to prevent redundant updates.
    - Updated order payment status and details upon successful validation.
    - Logged the incoming request body for debugging purposes.
    - Sent a successful response with updated order details.

---
+++ src/api/v1/payment/controller/ipnPayment.js
@@ -0,0 +1,63 @@
+const { PaymentStatus } = require("../../../../constants");
+const Order = require("../../../../models/Orders.model/Orders.model");
+const ApiError = require("../../../../utils/ApiError");
+const ApiResponse = require("../../../../utils/ApiResponse");
+const asyncHandler = require("../../../../utils/asyncHandler");
+
+const ipnPaymentController = asyncHandler(async (req, res) => {
+  const {
+    tran_id,
+    card_type,
+    card_no,
+    bank_tran_id,
+    currency,
+    card_issuer,
+    card_brand,
+    card_issuer_country,
+    tran_date,
+  } = req.body;
+  // Extract transaction ID from the request body
+
+  console.log(req.body);
+  if (!tran_id) {
+    throw new ApiError(400, "Transaction ID is required");
+  }
+
+  // Find the order with the given transaction ID
+  const findOrder = await Order.findOne({ transactionID: tran_id });
+
+  // If the order is not found, throw an error
+  if (!findOrder) {
+    throw new ApiError(404, `Order not found for transaction ID ${tran_id}`);
+  }
+
+  // If the order has already been paid, throw an error
+  if (findOrder.paymentStatus === PaymentStatus.SUCCEEDED) {
+    throw new ApiError(400, "Order already paid");
+  }
+
+  // Create an object to store payment details from the request body
+  const paymentType = {
+    card_type,
+    card_no,
+    bank_tran_id,
+    currency,
+    card_issuer,
+    card_brand,
+    card_issuer_country,
+    tran_date,
+  };
+
+  // Update the order's payment status and payment details
+  findOrder.paymentStatus = PaymentStatus.SUCCEEDED;
+  findOrder.paymentMethods = card_type;
+  findOrder.paymentType = paymentType;
+
+  // Save the updated order to the database
+  await findOrder.save();
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, { findOrder }, "Payment ipn successfully"));
+});
+module.exports = ipnPaymentController;
