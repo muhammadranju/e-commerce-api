28392a984fd4a623d5935e07ddd9f99db6109863
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-06-24 23:54:05 +0600

    feat: add orders routes to admin routes configuration

--- src/routes/admin.routes/index.js
+++ src/routes/admin.routes/index.js
@@ -4,7 +4,8 @@ const auth = require("./auth/register.routes");
 const seller = require("./seller/seller.routes");
 const store = require("./store/store.routes");
 const user = require("./user/users.routes");
+const orders = require("./orders/orders.routes");
 
-router.use([user, auth, seller, store]);
+router.use([user, auth, seller, store, orders]);
 
 module.exports = router;
