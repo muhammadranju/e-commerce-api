a414d2d82988bbeb480b0c54fb63a3dfe28da4c7
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-08-05 14:01:11 +0600

    feat: add seller authentication and profile management routes
    - Added POST route for seller registration with rate limiting and login checks.
    - Added GET route to fetch seller profile with authentication and ability checks.
    - Added GET route to fetch specific seller profile by ID with necessary permissions.
    - Added PATCH route to update specific seller profile by ID with proper access control.
    - Integrated middleware for authentication and ability checks to ensure secure access.

--- src/routes/admin.routes/seller/seller.routes.js
+++ src/routes/admin.routes/seller/seller.routes.js
@@ -1,3 +1,34 @@
-const router = require("express").Router();
+const { sellerAuthMiddleware } = require("../../../middleware/auth.middleware");
 
+const { controller: admin } = require("../../../api/v1/admin/sellers");
+const {
+  setAbilities,
+  canPerform,
+} = require("../../../middleware/restrictedMode");
+const rateLimiter = require("../../../utils/rateLimit.utils");
+const router = require("express").Router();
+const isLoginMiddleware = require(`../../../middleware/isSellerLoginMiddleware`);
+
+router
+  .route(`/seller/auth/register`)
+  .post(rateLimiter, isLoginMiddleware, admin.register);
+router.route(`/seller/profile`).get(
+  sellerAuthMiddleware,
+  setAbilities,
+  canPerform("read", "Seller")
+  // profile.findOne
+);
+router.route(`/seller/profile/:seller_id`).get(
+  sellerAuthMiddleware,
+  setAbilities,
+  canPerform("read", "Seller")
+  // profile.findSingle
+);
+
+router.route(`/seller/profile/:seller_id`).patch(
+  sellerAuthMiddleware,
+  setAbilities,
+  canPerform("update", "Seller")
+  // profile.update
+);
 module.exports = router;
